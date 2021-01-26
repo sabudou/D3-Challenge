f368267464a0a612c73aea1ffcfcaff9a9903abc
sabudou <sabudou@gmail.com>
2021-01-26 00:48:35 -0600

    add

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // @TODO: YOUR CODE HERE!
+
 // svg container
 var svgHeight = 400;
 var svgWidth = 1000;
@@ -10,15 +11,66 @@ var margin = { top: 50, right: 50, bottom: 50, left: 50 };
 var chartHeight = svgHeight - margin.top - margin.bottom;
 var chartWidth = svgWidth - margin.left - margin.right;
 
-// create svg container
-var svg = d3.select("#scatter").append("svg")
-  .attr("height", svgHeight)
-  .attr("width", svgWidth);
+var svg = d3
+  .select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
 
 var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-// Import data from an external CSV file
-d3.csv("data.csv").then(function(data) {
-    console.log(data);
-    console.log([data]);
+d3.csv("data.csv").then(function(datas) {
+  
+  console.log(datas);
+  
+  datas.forEach(function(data) {
+    data.age = +data.age;
+    data.smokes = +data.smokes;  
+  });
+ 
+// Create a scale for your independent (x) coordinates
+  var xScale = d3.scaleLinear()
+    .domain(d3.extent(datas, d => d.age))
+    .range([0, chartWidth])
+    .nice();
+
+// Create a scale for your dependent (y) coordinates
+  var yScale = d3.scaleLinear()
+    .domain([6,d3.max(datas, d => d.smokes)])
+    .range([chartHeight, 0])
+  
+  var xAxis = d3.axisBottom(xScale);
+  var yAxis = d3.axisLeft(yScale);
+
+  chartGroup.append("g").attr("transform", `translate(0, ${chartHeight})`).call(xAxis);
+  chartGroup.append("g").call(yAxis);
+
+// append circles
+
+chartGroup.selectAll("circle")
+.data(datas)
+.enter()
+.append("circle")
+.attr("cx", d => xScale(d.age))
+.attr("cy", d => yScale(d.smokes))
+.attr("r", "10")
+.attr("stroke-width", "1")
+
+chartGroup.append("g")
+  .selectAll('text')
+  .data(datas)
+  .enter()
+  .append("text")
+  .text(d=>d.abbr)
+  .attr("x",d=>xScale(d.age))
+  .attr("y",d=>yScale(d.smokes))
+  .classed(".stateText", true)
+  .attr("text-anchor", "middle")
+  .attr("fill", "white")
+  .attr("font-size", "10px")
+  .style("font-weight", "bold");
+    
+}).catch(function(error) {
+  console.log(error);
+});
