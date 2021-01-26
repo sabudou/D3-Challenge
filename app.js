cc9cdad7840d731593d53494536f4eb350270425
sabudou <sabudou@gmail.com>
2021-01-25 23:26:49 -0600

    add

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 // @TODO: YOUR CODE HERE!
+// svg container
+var svgHeight = 400;
+var svgWidth = 1000;
+
+// margins
+var margin = { top: 50, right: 50, bottom: 50, left: 50 };
+
+// chart area minus margins
+var chartHeight = svgHeight - margin.top - margin.bottom;
+var chartWidth = svgWidth - margin.left - margin.right;
